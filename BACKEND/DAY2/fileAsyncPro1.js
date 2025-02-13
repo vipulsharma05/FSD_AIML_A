8a51ffaa1bda6cf0742f9aea087eae6286cbe4e8
VIPULSHARMA <vipulsharma5002@gmail.com>
2025-02-13 11:11:04 +0530

    hgya

--- BACKEND/DAY2/fileAsyncPro1.js
+++ BACKEND/DAY2/fileAsyncPro1.js
@@ -1,9 +1,17 @@
-const fs=require(fs/Promise);
+
+const fs=require('fs/promises');
 function myreadFile(){
     try{
-
+        const res=fs.readFile('dummy.txt','utf-8');
+        res.then((data)=>{
+            console.log("file data",data)
+        })
+        .catch((err)=>{
+            throw err;
+        })
     }
     catch(err){
         console.log('File Reading error:',err.messgae);
     }
 }
+myreadFile();
